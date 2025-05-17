5ec35288dc18f8b0fc10eaeeb21ae01767ff8e82
Sean Dempsey <dempsey.sean@outlook.com>
2025-05-17 16:15:34 -0700

    Safer handling of vanilla items

--- ArchipelagoMapMod/RC/APRandoContext.cs
+++ ArchipelagoMapMod/RC/APRandoContext.cs
@@ -24,23 +24,27 @@ public class APRandoContext : RandoContext
         this.StartDef = startDef;
 
         Vanilla = [];
-        foreach (PoolDef pool in Data.Pools)
+        // if unshuffled locations were added, the server will send all vanilla placements as randomized instead
+        if (!ArchipelagoMod.Instance.SlotData.Options.AddUnshuffledLocations)
         {
-            if (pool.IsVanilla(gs))
+            foreach (PoolDef pool in Data.Pools)
             {
-                // grimmchild gets the first 6 flames free so don't add them
-                if (pool.Name == PoolNames.Flame && gs.PoolSettings.Charms)
+                if (pool.IsVanilla(gs))
                 {
-                    foreach (VanillaDef def in pool.Vanilla.Skip(6))
+                    // grimmchild gets the first 6 flames free so don't add them
+                    if (pool.Name == PoolNames.Flame && gs.PoolSettings.Charms)
                     {
-                        Vanilla.Add(MakeVanillaItemPlacement(def));
+                        foreach (VanillaDef def in pool.Vanilla.Skip(6))
+                        {
+                            Vanilla.Add(MakeVanillaItemPlacement(def));
+                        }
                     }
-                }
-                else
-                {
-                    foreach (VanillaDef def in pool.Vanilla)
+                    else
                     {
-                        Vanilla.Add(MakeVanillaItemPlacement(def));
+                        foreach (VanillaDef def in pool.Vanilla)
+                        {
+                            Vanilla.Add(MakeVanillaItemPlacement(def));
+                        }
                     }
                 }
             }
@@ -72,33 +76,30 @@ public class APRandoContext : RandoContext
                 if (!item.GetTag(out ArchipelagoItemTag aptag)) continue;
 
                 APLocation logicLocation = new(LM.GetLogicDef(placement.Name));
+
+                placement.GetOrAddTag<APmmPlacementTag>();
+                APmmItemTag itemTag = item.GetOrAddTag<APmmItemTag>();
+
+                APItem logicItem;
                     
-                if ((bool) GenerationSettings.Get($"PoolSettings.{SupplementalMetadata.Of(placement).Get(InjectedProps.LocationPoolGroup).Replace(" ", "")}"))
+                // create new APItem and attach a logic item to it if its logically relevant
+                if (aptag.IsItemForMe)
                 {
-                    placement.GetOrAddTag<APmmPlacementTag>();
-                    APmmItemTag itemTag = item.GetOrAddTag<APmmItemTag>();
-
-                    APItem logicItem;
-                    
-                    // create new APItem and attach a logic item to it if its logically relevant
-                    if (aptag.IsItemForMe)
+                    logicItem = new APItem(item.name)
                     {
-                        logicItem = new APItem(item.name)
-                        {
-                            item = LM.GetItem(item.name)
-                        };
-                        ArchipelagoMapMod.Instance.LogDebug($"[AP RC] Adding Randomized self-item {item.name} to logic context at {placement.Name}.");
-                    }
-                    else
-                    {
-                        // create an EmptyItem that has no logic terms attached to it.
-                        logicItem = new APItem(item.name + "_player_" + aptag.Player);
-                        ArchipelagoMapMod.Instance.LogDebug($"[AP RC] Adding Randomized other-item {item.name} to logic context as {logicItem.Name} at {placement.Name}.");
-                    }
-
-                    itemTag.id = ItemPlacements.Count;
-                    ItemPlacements.Add(new ItemPlacement(logicItem, logicLocation));
+                        item = LM.GetItem(item.name)
+                    };
+                    ArchipelagoMapMod.Instance.LogDebug($"[AP RC] Adding Randomized self-item {item.name} to logic context at {placement.Name}.");
                 }
+                else
+                {
+                    // create an EmptyItem that has no logic terms attached to it.
+                    logicItem = new APItem(item.name + "_player_" + aptag.Player);
+                    ArchipelagoMapMod.Instance.LogDebug($"[AP RC] Adding Randomized other-item {item.name} to logic context as {logicItem.Name} at {placement.Name}.");
+                }
+
+                itemTag.id = ItemPlacements.Count;
+                ItemPlacements.Add(new ItemPlacement(logicItem, logicLocation));
             }
         }
         for (int i = 0; i < ItemPlacements.Count; i++)
